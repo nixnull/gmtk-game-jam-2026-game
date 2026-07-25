68e08f54ffcb689a8ecfbdf55423d26a1d5ddb5c
rcvines <68253433+rcvines@users.noreply.github.com>
2026-07-24 19:49:40 -0500

    adds bgm
    adds bgm. starts intro on _ready(), plays the loop version when it finishes.

--- main_screen.gd
+++ main_screen.gd
@@ -3,6 +3,7 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$"StartScreen".visible = true
+	$"IntroBGM".play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
