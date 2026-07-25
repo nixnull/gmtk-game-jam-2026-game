bf0c5c944c60d2ea573d9281d7a7def7a1557482
rcvines <68253433+rcvines@users.noreply.github.com>
2026-07-24 20:33:27 -0500

    fix bgm
    god i hope this fixed it. move the signal processing from introbgm to loopbgm

--- loop_bgm.gd
+++ loop_bgm.gd
@@ -9,3 +9,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+
+func _on_intro_bgm_finished() -> void:
+	play()
+	
